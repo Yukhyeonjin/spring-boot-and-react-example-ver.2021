9c39470a70a91f1b07a9ad1a2dab321125b1470a
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-26 17:47:52 +0900

    [2021.12.26 YHJ]
    1. 조회 추가

--- src/main/java/com/yukhyunjin/studentsystem/controller/StudentController.java
+++ src/main/java/com/yukhyunjin/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.yukhyunjin.studentsystem.controller;
 import com.yukhyunjin.studentsystem.model.Student;
 import com.yukhyunjin.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New Student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/yukhyunjin/studentsystem/service/StudentService.java
+++ src/main/java/com/yukhyunjin/studentsystem/service/StudentService.java
@@ -3,8 +3,11 @@ package com.yukhyunjin.studentsystem.service;
 import com.yukhyunjin.studentsystem.model.Student;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface StudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 
 }
